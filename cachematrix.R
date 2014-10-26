58c3df6c4ffb9aa69e1fcc6ff16ace8339c1ba2d
Allan McLemore <allan@allanmclemore.com>
2014-10-26 16:24:36 -0500

    Completed Assignment
    Completed Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+# Summary - Returns an instance of a matrix that has the ability to cache it's Inverse
+#
+# Input Parameters:
+# x - the matrix instance contained in the function used to compute the inverse matrix
+#
+# Public Methods:
+# Value    - returns the value of the matrix
+# Inverse  - returns the Inverse of the matrix from the "cache"
+# Solve    - computes the Inverse of the matrix and stores it in the "cache"
 makeCacheMatrix <- function(x = matrix()) {
-
+  matrixValue <- x
+  inverse <- NULL
+  list(
+    Value = function(){
+      matrixValue 
+    },
+    Inverse = function(){
+      inverse
+    },
+    Solve = function(){
+      cat("Calculating inverse of matrix...")
+      inverse <<- solve(matrixValue) %*% matrixValue
+    }
+  )
 }
 
-
-## Write a short comment describing this function
-
+# Summary - Computes the inverse of the supplied matrix contained in the makeCacheMatrix function instance
+# 
+# Input Parameters 
+# x - instance of makeCacheMatrix
+#
+# Returns
+# inverse of the supplied matrix contained in the makeCacheMatrix function
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  if(is.null(x$Inverse())){
+    x$Solve()
+  }
+  x$Inverse()
 }
+
+# Main Program -   Lines below are the main program that verify the above functions are coded to spec
+# 
+# First call: should warm cache, perform inverse calc, cache calc, and return Inverse of matrix
+# Second call: should return Inverse of matrix from cache
+# So debug message 'Calculating inverse of matrix...' should only print one time when this code runs
+cMatrix = makeCacheMatrix(matrix(c(1, 0, 0, 1), nrow = 2, ncol = 2, byrow = TRUE))
+cat("Inverse is: ")
+cat(cacheSolve(cMatrix))
+cat("Inverse is: ")
+cat(cacheSolve(cMatrix))
+
